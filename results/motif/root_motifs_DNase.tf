b16974995cd52a6cd769eb63a876cb28c57db3d0
Raphael Mourad <raphael.mourad@ibcg.biotoul.fr>
2025-02-10 09:31:14 +0100

    Add files via upload

---
+++ results/motif/root_motifs_DNase.tf
@@ -0,0 +1,311 @@
+AC  cluster_3
+XX
+ID  cluster_3
+XX
+DE  rgccCwGG
+P0           a         c         g         t
+1         1122       158       968       130
+2          158       766      2688       625
+3          309      2646       466       816
+4          679      2188       316      1054
+5          194      2898       920       225
+6         2468       406       268      1095
+7          136       169      3274       658
+8          122        76      1522       139
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_3
+CC  AC: cluster_3
+CC  id: cluster_3
+CC  name: cluster_3
+CC  version: 
+CC  name: cluster_3
+CC  description: rgccCwGG
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: agccCaGG
+CC  consensus.strict.rc: CCTGGGCT
+CC  consensus.IUPAC: rgccCwGG
+CC  consensus.IUPAC.rc: CCWGGGCY
+CC  consensus.regexp: [ag]gccC[at]GG
+CC  consensus.regexp.rc: CC[AT]GGGC[CT]
+CC  type: merge
+CC  merge_type: sum
+CC  merge_nb: 3
+CC  merged_AC: 'raf4:::Unknown,raf4:::Unknown,raf4:::Unknown'
+CC  merged_ID: 'kernel96,kernel92,kernel124'
+XX
+//
+AC  cluster_4
+XX
+ID  cluster_4
+XX
+DE  Unknown kernel3; from JASPAR
+P0           a         c         g         t
+1          158       165      1789        25
+2          111       321      1300       405
+3          203      1135       667       132
+4          548       980       233       376
+5          120      1104       496       417
+6          214      1059       436       428
+7          540       751       431       415
+8          194      1111       700       132
+9          299      1213       539        86
+10        1411       273       269       184
+11          55        74      1586       422
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_4
+CC  AC: raf4_m2_Unknown
+CC  id: raf4_m2_Unknown
+CC  name: cluster_4
+CC  version: 
+CC  name: cluster_4
+CC  description: Unknown kernel3; from JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  merge_nb: 1
+CC  type: merge
+CC  merged_AC: raf4:::kernel3
+CC  merged_ID: kernel3
+CC  merge_type: sum
+XX
+//
+AC  cluster_5
+XX
+ID  cluster_5
+XX
+DE  Unknown kernel2; from JASPAR
+P0           a         c         g         t
+1           50       144      2132       121
+2          369      1176       790       112
+3           98      1600       363       386
+4          893      1049       165       340
+5           80      1632       530       205
+6          567      1565       104       211
+7          668       540       536       703
+8          222       756       617       852
+9          390       985       688       384
+10         176      1900       160       211
+11         106      2120        57       164
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_5
+CC  AC: raf4_m1_Unknown
+CC  id: raf4_m1_Unknown
+CC  name: cluster_5
+CC  version: 
+CC  name: cluster_5
+CC  description: Unknown kernel2; from JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  merged_ID: kernel2
+CC  merge_type: sum
+CC  merged_AC: raf4:::kernel2
+CC  type: merge
+CC  merge_nb: 1
+XX
+//
+AC  cluster_2
+XX
+ID  cluster_2
+XX
+DE  CtCwgsvhccCCAG
+P0           a         c         g         t
+1          243      2363        98       154
+2          307       613       214      1724
+3          592      8112      1335       690
+4         3520      1394       870      4945
+5         1038      1081      7028      1582
+6         1069      3033      4336      2291
+7         2698      3028      3571      1432
+8         2855      2891      1862      3121
+9         1257      5118      1692      2662
+10         995      6406       958      2370
+11        1252      7615       607       751
+12         350      5788       193       187
+13        2518       124       176       579
+14          57       114      1358        38
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_2
+CC  AC: cluster_2
+CC  id: cluster_2
+CC  name: cluster_2
+CC  version: 
+CC  name: cluster_2
+CC  description: CtCwgsvhccCCAG
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: CtCtgggtccCCAG
+CC  consensus.strict.rc: CTGGGGACCCAGAG
+CC  consensus.IUPAC: CtCwgsvhccCCAG
+CC  consensus.IUPAC.rc: CTGGGGDBSCWGAG
+CC  consensus.regexp: CtC[at]g[cg][acg][act]ccCCAG
+CC  consensus.regexp.rc: CTGGGG[AGT][CGT][CG]C[AT]GAG
+CC  type: merge
+CC  merge_type: sum
+CC  merge_nb: 6
+CC  merged_AC: 'raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown'
+CC  merged_ID: 'kernel76,kernel83,kernel117,kernel15,kernel81,kernel128'
+XX
+//
+AC  cluster_6
+XX
+ID  cluster_6
+XX
+DE  Unknown kernel38; from JASPAR
+P0           a         c         g         t
+1           10       529        12        11
+2          449        18         4        91
+3           92       180       227        63
+4          172       129       172        89
+5          307        30       175        50
+6           59        60       214       229
+7          111       253       164        34
+8           90        23        10       439
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_6
+CC  AC: raf4_m11_Unknown
+CC  id: raf4_m11_Unknown
+CC  name: cluster_6
+CC  version: 
+CC  name: cluster_6
+CC  description: Unknown kernel38; from JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  merged_ID: kernel38
+CC  merged_AC: raf4:::kernel38
+CC  merge_type: sum
+CC  type: merge
+CC  merge_nb: 1
+XX
+//
+AC  cluster_7
+XX
+ID  cluster_7
+XX
+DE  Unknown kernel16; from JASPAR
+P0           a         c         g         t
+1           84      1385       189        82
+2         1464        89        68       119
+3           31       174      1320       215
+4          238       663       666       173
+5           91      1087       382       180
+6          391       258       293       798
+7          145        49      1329       217
+8          142        36      1426       136
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_7
+CC  AC: raf4_m6_Unknown
+CC  id: raf4_m6_Unknown
+CC  name: cluster_7
+CC  version: 
+CC  name: cluster_7
+CC  description: Unknown kernel16; from JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  merged_AC: raf4:::kernel16
+CC  merge_type: sum
+CC  merged_ID: kernel16
+CC  merge_nb: 1
+CC  type: merge
+XX
+//
+AC  cluster_8
+XX
+ID  cluster_8
+XX
+DE  Unknown kernel31; from JASPAR
+P0           a         c         g         t
+1           30       518        45        32
+2          466        75        17        67
+3          173       197       186        69
+4          315        37       175        98
+5          111        78       379        57
+6          248        29        76       272
+7           28        79       345       173
+8           12       336        59       218
+9          105       156       123       241
+10         211        24       254       136
+11         140         9       450        26
+12          45       104       430        46
+13         520        29        45        31
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_8
+CC  AC: raf4_m9_Unknown
+CC  id: raf4_m9_Unknown
+CC  name: cluster_8
+CC  version: 
+CC  name: cluster_8
+CC  description: Unknown kernel31; from JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  merge_nb: 1
+CC  merged_AC: raf4:::kernel31
+CC  merge_type: sum
+CC  type: merge
+CC  merged_ID: kernel31
+XX
+//
+AC  cluster_1
+XX
+ID  cluster_1
+XX
+DE  CGcgscssrygCGCG
+P0           a         c         g         t
+1          501      5004      1314       197
+2          470       833      5230       483
+3         1096      8376      2251      1181
+4         1679      2910      6876      1439
+5         2910      5646      4579      1816
+6         4384     15520      4591      2924
+7         4212     10191     17305      3362
+8         5385     13252     11068      5365
+9         8873      7518     10172      8507
+10        6173      9886      8525     10486
+11        6226      7554     15592      5698
+12        2348     23973      5781      2968
+13        2683      1836     27244      1952
+14         298     11109       497       357
+15         344       592     10811       514
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_1
+CC  AC: cluster_1
+CC  id: cluster_1
+CC  name: cluster_1
+CC  version: 
+CC  name: cluster_1
+CC  description: CGcgscssrygCGCG
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: CGcgccgcgtgCGCG
+CC  consensus.strict.rc: CGCGCACGCGGCGCG
+CC  consensus.IUPAC: CGcgscssrygCGCG
+CC  consensus.IUPAC.rc: CGCGCRYSSGSCGCG
+CC  consensus.regexp: CGcg[cg]c[cg][cg][ag][ct]gCGCG
+CC  consensus.regexp.rc: CGCGC[AG][CT][CG][CG]G[CG]CGCG
+CC  type: merge
+CC  merge_type: sum
+CC  merge_nb: 16
+CC  merged_AC: 'raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown,raf4:::Unknown'
+CC  merged_ID: 'kernel4,kernel98,kernel120,kernel19,kernel70,kernel99,kernel121,kernel44,kernel42,kernel27,kernel43,kernel84,kernel48,kernel58,kernel35,kernel7'
+XX
+//
